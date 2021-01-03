8f63f035aff059e86fa823a73f5213a5fbd3508f
fiddesidde <fredrik.mellberg@gmail.com>
2021-01-03 23:15:42 +0100

    Add exercises up to incl 2.12

---
+++ part2/countries/src/App.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+
+const Country = ({ country }) => <div>{country.name}</div>;
+
+const Language = ({ language }) => <li>{language}</li>;
+
+const CountryData = ({ country }) => {
+    return (
+        <>
+            <h2>{country.name}</h2>
+            <div>Capital: {country.capital}</div>
+            <div>Population: {country.population}</div>
+            <h3>Languages</h3>
+            <ul>
+                {country.languages.map(language => (
+                    <Language
+                        key={language.iso639_2}
+                        language={language.name}
+                    />
+                ))}
+            </ul>
+            <img src={country.flag} alt="country flag" id="flag" />
+        </>
+    );
+};
+
+const Countries = ({ countries }) => {
+    if (countries.length === 1) {
+        return <CountryData country={countries[0]} />;
+    } else if (countries.length <= 10) {
+        return countries.map(country => (
+            <Country key={country.name} country={country} />
+        ));
+    }
+    return 'Too many matches, specify another filter';
+};
+
+const App = () => {
+    const [countries, setCountries] = useState([]);
+    const [filter, setFilter] = useState('');
+
+    useEffect(() => {
+        axios.get('https://restcountries.eu/rest/v2/all').then(res => {
+            setCountries(res.data);
+        });
+    }, []);
+
+    const handleFilterChange = event => {
+        setFilter(event.target.value);
+    };
+
+    const filteredCountries = countries.filter(country =>
+        country.name.toLowerCase().includes(filter.toLowerCase())
+    );
+
+    return (
+        <>
+            <h1>Countries</h1>
+            <div>
+                Filter countries:{' '}
+                <input value={filter} onChange={handleFilterChange} />
+            </div>
+            <div>
+                <Countries
+                    key={filteredCountries.name}
+                    countries={filteredCountries}
+                />
+            </div>
+        </>
+    );
+};
+
+export default App;
