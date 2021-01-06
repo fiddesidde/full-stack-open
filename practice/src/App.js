a002c51dcb7f86d048c68e9b35d4e4eea75a4930
fiddesidde <fredrik.mellberg@gmail.com>
2021-01-06 20:21:46 +0100

    Add practice up to 2.e

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Note from './components/Note';
+import noteService from './services/notes';
 
 const App = () => {
     const [notes, setNotes] = useState([]);
@@ -8,29 +9,49 @@ const App = () => {
     const [showAll, setShowAll] = useState(true);
 
     useEffect(() => {
-        console.log('effect');
-        axios.get('http://localhost:3001/notes').then(res => {
-            console.log('promise fullfilled');
-            setNotes(res.data);
+        noteService.getAll().then(initialNotes => {
+            setNotes(initialNotes);
         });
     }, []);
-    console.log('render', notes.length, 'notes');
 
     const addNote = event => {
         event.preventDefault();
         const noteObject = {
+            id: notes.length + 1,
             content: newNote,
             date: new Date().toISOString(),
             important: Math.random() < 0.5,
-            id: notes.length + 1,
         };
+
+        noteService.create(noteObject).then(returnedNote => {
+            setNotes(notes.concat(returnedNote));
+            setNewNote('');
+        });
+
         setNotes(notes.concat(noteObject));
         setNewNote('');
-        console.log('button clicked', event.target);
+    };
+
+    const toggleImportanceOf = id => {
+        const note = notes.find(n => n.id === id);
+        const changedNote = { ...note, important: !note.important };
+
+        noteService
+            .update(id, changedNote)
+            .then(returnedNote => {
+                setNotes(
+                    notes.map(note => (note.id !== id ? note : returnedNote))
+                );
+            })
+            .catch(err => {
+                alert(
+                    `the note "${note.content}" was already deleted from the server`
+                );
+                setNotes(notes.filter(n => n.id !== id));
+            });
     };
 
     const handleNoteChange = event => {
-        console.log(event.target.value);
         setNewNote(event.target.value);
     };
 
@@ -46,7 +67,11 @@ const App = () => {
             </div>
             <ul>
                 {notesToShow.map(note => (
-                    <Note key={note.id} note={note} />
+                    <Note
+                        key={note.id}
+                        note={note}
+                        toggleImportance={() => toggleImportanceOf(note.id)}
+                    />
                 ))}
             </ul>
             <form onSubmit={addNote}>
---
+++ practice/src/services/notes.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+const baseUrl = 'http://localhost:3001/notes';
+
+// const getAll = () => {
+//     const request = axios.get(baseUrl);
+//     return request.then(res => res.data);
+// };
+
+const getAll = () => {
+    const request = axios.get(baseUrl);
+    const nonExisting = {
+        id: 10000,
+        content: 'This note is note saved to server',
+        date: '2020-01-05T18:00:00.098Z',
+        important: true,
+    };
+    return request.then(res => res.data.concat(nonExisting));
+};
+
+const create = newObject => {
+    const request = axios.post(baseUrl, newObject);
+    return request.then(res => res.data);
+};
+
+const update = (id, newObject) => {
+    const request = axios.put(`${baseUrl}/${id}`, newObject);
+    return request.then(res => res.data);
+};
+
+export default {
+    // Shorthand for getAll: getAll, ...
+    getAll,
+    create,
+    update,
+};
