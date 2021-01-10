b0e34df30698066cd2233d24a5a15b793f6b45fc
fiddesidde <fredrik.mellberg@gmail.com>
2021-01-10 20:48:37 +0100

    Add practice 'Middleware'

--- note-backend/index.js
+++ note-backend/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.use(express.json());
-
 let notes = [
     {
         id: 1,
@@ -29,6 +27,21 @@ const generateId = () => {
         notes.length > 0 ? Math.max(...notes.map(note => note.id)) : 0;
     return maxId + 1;
 };
+
+const requestLogger = (req, res, next) => {
+    console.log('Method:', req.method);
+    console.log('Path:  ', req.path);
+    console.log('Body:  ', req.body);
+    console.log('---');
+    next();
+};
+
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.use(express.json());
+app.use(requestLogger);
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World</h1>');
@@ -71,6 +84,8 @@ app.post('/api/notes', (req, res) => {
     notes = notes.concat(note);
     res.json(note);
 });
+
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
