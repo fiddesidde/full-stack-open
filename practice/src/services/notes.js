152faa8a8cb5bbc71f72fd0a3c1bdb9904f91795
fiddesidde <fredrik.mellberg@gmail.com>
2021-01-11 06:28:26 +0100

    Modify baseUrl

--- practice/src/services/notes.js
+++ practice/src/services/notes.js
@@ -1,21 +1,21 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:3001/notes';
-
-// const getAll = () => {
-//     const request = axios.get(baseUrl);
-//     return request.then(res => res.data);
-// };
+const baseUrl = 'http://localhost:3001/api/notes';
 
 const getAll = () => {
     const request = axios.get(baseUrl);
-    const nonExisting = {
-        id: 10000,
-        content: 'This note is note saved to server',
-        date: '2020-01-05T18:00:00.098Z',
-        important: true,
-    };
-    return request.then(res => res.data.concat(nonExisting));
+    return request.then(res => res.data);
 };
+
+// const getAll = () => {
+//     const request = axios.get(baseUrl);
+//     const nonExisting = {
+//         id: 10000,
+//         content: 'This note is note saved to server',
+//         date: '2020-01-05T18:00:00.098Z',
+//         important: true,
+//     };
+//     return request.then(res => res.data.concat(nonExisting));
+// };
 
 const create = newObject => {
     const request = axios.post(baseUrl, newObject);
