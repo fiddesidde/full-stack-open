79c1961e28cf23581e2eccd6c741fc930659c278
fiddesidde <fredrik.mellberg@gmail.com>
2021-01-12 09:48:45 +0100

    Add refactor exercise 4.2

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -1,52 +1,9 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-}
-
+const app = require('./app');
 const http = require('http');
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
+const { PORT } = require('./utils/config');
 
-const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number,
-});
+const server = http.createServer(app);
 
-const Blog = mongoose.model('Blog', blogSchema);
-
-const mongoUrl = process.env.DB_URL;
-mongoose.connect(mongoUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-});
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'error connecting to MongoDB:'));
-db.once('open', () => {
-    console.info('Connected to MongoDB');
-});
-
-app.use(cors());
-app.use(express.json());
-
-app.get('/api/blogs', async (req, res) => {
-    const blogs = await Blog.find({});
-    res.json(blogs);
-});
-
-app.post('/api/blogs', async (req, res) => {
-    const blog = new Blog(req.body);
-
-    result = await blog.save();
-    res.status(201).json(result);
-});
-
-const PORT = 3003;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
