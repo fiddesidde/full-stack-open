ed1f21fb82bef63f65bed51c88d6e8f3ea65cb8b
fiddesidde <fredrik.mellberg@gmail.com>
2021-01-14 21:21:59 +0100

    Add practice for part 4 complete

--- note-backend/controllers/notes.js
+++ note-backend/controllers/notes.js
@@ -1,8 +1,13 @@
 const notesRouter = require('express').Router();
 const Note = require('../models/note');
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 notesRouter.get('/', async (req, res) => {
-    const notes = await Note.find({});
+    const notes = await Note.find({}).populate('user', {
+        username: 1,
+        name: 1,
+    });
     res.json(notes);
 });
 
@@ -15,14 +20,34 @@ notesRouter.get('/:id', async (req, res) => {
     }
 });
 
+const getTokenFrom = req => {
+    const authorization = req.get('authorization');
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7);
+    }
+    return null;
+};
+
 notesRouter.post('/', async (req, res) => {
     const body = req.body;
+    const token = getTokenFrom(req);
+    const decodedToken = jwt.verify(token, process.env.SECRET);
+    if (!token || !decodedToken.id) {
+        return res.status(401).json({ error: 'token missing or invalid' });
+    }
+    const user = await User.findById(decodedToken.id);
+
     const note = new Note({
         content: body.content,
-        important: body.important || false,
+        important: body.important === undefined ? false : body.important,
         date: new Date(),
+        user: user.id,
     });
+
     const savedNote = await note.save();
+    user.notes = user.notes.concat(savedNote._id);
+    await user.save();
+
     res.json(savedNote);
 });
 
--- note-backend/utils/middleware.js
+++ note-backend/utils/middleware.js
@@ -17,10 +17,12 @@ const errorHandler = (error, req, res, next) => {
 
     if (error.name === 'CastError') {
         return res.status(400).send({ error: 'malformatted id' });
-    }
-    if (error.name === 'ValidationError') {
+    } else if (error.name === 'ValidationError') {
         return res.status(400).json({ error: error.message });
+    } else if (error.name === 'JsonWebTokenError') {
+        return res.status(401).json({ error: 'invalid token' });
     }
+    logger.error(error.message);
     next(error);
 };
 
